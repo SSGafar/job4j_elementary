899e6114fa444b07e8df45d649ece439748da4fe
SSGafar <gafar1983@mail.ru>
2025-08-15 13:47:10 +0500

    Модульные тесты [#505277]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -1,14 +1,9 @@
 package ru.job4j.converter;
-
 public class Converter {
-
-        public static double rubleToEuro(double value) {
+    public static double rubleToEuro(double value) {
             return value / 70;
-        }
-
+    }
         public static double rubleToDollar(double  value) {
             return value / 60;
         }
     }
-
-

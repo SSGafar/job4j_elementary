39f39d1b1c77695187126ae6908eb140e79494c5
SSGafar <gafar1983@mail.ru>
2025-08-15 16:24:06 +0500

    Модульные тесты [#505277]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -1,9 +1,11 @@
 package ru.job4j.converter;
+
 public class Converter {
     public static double rubleToEuro(double value) {
-            return value / 70;
+        return value / 70;
     }
-        public static double rubleToDollar(double  value) {
-            return value / 60;
-        }
+
+    public static double rubleToDollar(double value) {
+        return value / 60;
     }
+}
--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -1,6 +1,9 @@
 package ru.job4j.converter;
+
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.*;
+
 class ConverterTest {
     @Test
     void whenConvert140RblThen2Euro() {
@@ -10,6 +13,7 @@ class ConverterTest {
         double value = 0.0001f;
         assertThat(output).isEqualTo(expected, withPrecision(value));
     }
+
     @Test
     void whenConvert180RblThen3Dollar() {
         double input = 180;
